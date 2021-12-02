eeb23153f14616cdcb3353694e75a7d83af666c3
JoshR <atticus183@gmail.com>
2021-12-02 12:27:14 -0600

    Adds unit testing target

---
+++ ServiceLayerExample/Services/MockJsonPlaceholderService.swift
@@ -0,0 +1,16 @@
+//
+//  MockJsonPlaceholderService.swift
+//  ServiceLayerExampleTests
+//
+//  Created by Josh Rondestvedt on 12/2/21.
+//
+
+import Foundation
+
+@testable import ServiceLayerExample
+
+//final class MockJsonPlaceholderService: JsonPlaceholderServiceProtocol {
+//    func fetch<T>(_ endpoint: JsonPlaceholderService.Endpoint, completion: @escaping (Result<[T], Error>) -> Void) where T : Decodable, T : Encodable {
+//        <#code#>
+//    }
+//}
